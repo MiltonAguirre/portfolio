f779276ad20077263056bb35874b60a3c60fbadd
MiltonAguirre <milton.aguirre92@gmail.com>
2020-11-30 17:57:17 -0300

    Init git proyect

---
+++ resources/views/home.blade.php
@@ -0,0 +1,12 @@
+
+@section('content')
+<div class="container">
+  <div class="col-10 float-center justify-content-center">
+    <h1>PortFolio - Milton Aguirre</h1>
+  </div>
+  <div class="col-10">
+    <h2>Desarrollo WEB y Móvil</h2>
+  </div>
+
+</div>
+@endsection
