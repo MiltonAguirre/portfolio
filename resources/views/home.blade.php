64436055d72e525198e5798c0e330bb9a9afd0fc
MiltonAguirre <milton.aguirre92@gmail.com>
2020-11-30 18:21:55 -0300

    1 - Laravel UI

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,12 +1,23 @@
+@extends('layouts.app')
 
 @section('content')
 <div class="container">
-  <div class="col-10 float-center justify-content-center">
-    <h1>PortFolio - Milton Aguirre</h1>
-  </div>
-  <div class="col-10">
-    <h2>Desarrollo WEB y Móvil</h2>
-  </div>
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header">{{ __('Dashboard') }}</div>
 
+                <div class="card-body">
+                    @if (session('status'))
+                        <div class="alert alert-success" role="alert">
+                            {{ session('status') }}
+                        </div>
+                    @endif
+
+                    {{ __('You are logged in!') }}
+                </div>
+            </div>
+        </div>
+    </div>
 </div>
 @endsection
